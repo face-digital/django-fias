7731f11806e786bf6773ec98f26fcb84367f83ba
vage-mel <vage.melikbekyan@gmail.com>
2024-06-05 12:11:03 +0300

    fix url
    fix

--- fias/suggest/backends/noop/urls.py
+++ fias/suggest/backends/noop/urls.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import *
 
 
 urlpatterns = [
-    url(r'^suggest.json$', EmptyResponseListView.as_view(), name='suggest'),
-    url(r'^suggest-area.json$', EmptyResponseListView.as_view(), name='suggest-area'),
+    re_path(r'^suggest.json$', EmptyResponseListView.as_view(), name='suggest'),
+    re_path(r'^suggest-area.json$', EmptyResponseListView.as_view(), name='suggest-area'),
 ]
